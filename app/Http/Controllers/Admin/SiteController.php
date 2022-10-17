e2c92bb425043730369994e90d1a0520c750ae8b
shahibarkan <70672872+shahibarkan@users.noreply.github.com>
2022-10-17 22:20:46 +0700

    Add files via upload

---
+++ app/Http/Controllers/Admin/SiteController.php
@@ -0,0 +1,33 @@
+<?php namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use App\Models\Day;
+use App\Models\Lecturer;
+use App\Models\Room;
+use App\Models\Schedule;
+use App\Models\Teach;
+use App\Models\Time;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Auth;
+
+class SiteController extends Controller
+{
+    public function index(Request $request)
+    {
+        // if(Auth::user()->role != 'admin') {
+        //     return redirect()->route('admin.generates.result', 1);
+        // }
+        $users     = User::count();
+        $courses   = Course::count();
+        $days      = Day::count();
+        $lecturers = Lecturer::count();
+        $rooms     = Room::count();
+        $teachs    = Teach::count();
+        $times     = Time::count();
+        $schedules = Schedule::count();
+
+        return view('admin.site.admin', compact('users', 'courses', 'days', 'lecturers', 'rooms', 'teachs', 'times', 'schedules'));
+    }
+}
